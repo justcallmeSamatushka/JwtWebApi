e944d181296de7c77446c1cb2b19782681669221
Samat <s.toichuev@innopolis.ru>
2022-06-23 12:07:42 +0600

    Add project files.

---
+++ JwtWebApi/User.cs
@@ -0,0 +1,10 @@
+﻿namespace JwtWebApi
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+
+    }
+}
